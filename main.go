b4bb967a884b0feabe87cf8240ba89ccf0f71d08
Taras <147646967+Mikki535@users.noreply.github.com>
2024-03-18 14:03:55 +0200

    Add coments

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 	"net/smtp"
 )
 
+// templates:
 var tpl = template.Must(template.ParseFiles("templates/index.html"))
 var tplp = template.Must(template.ParseFiles("templates/about.html"))
 
 func main() {
 	http.HandleFunc("/about", about)
 	http.HandleFunc("/", HomeFunc)
+	//start server on localhost :8080
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -20,12 +22,15 @@ func HomeFunc(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		gmail := r.FormValue("gmail")
 		message := r.FormValue("message")
-
+		// dont forget change ENTERGMAIL@gmail.com on real gmail!
+		// authentication in SMTP // Don`t forget change secret code!!!
 		auth := smtp.PlainAuth("", "ENTERGMAIL@gmail.com", "YOU-SECRET-CODE", "smtp.gmail.com")
 
 		to := []string{gmail}
 		msg := message
+		// send message on gmail
 
+		// don`t forget change ENTERGMAIL@gmail.com on real gmail!
 		err := smtp.SendMail("smtp.gmail.com:587", auth, "ENTERGMAIL@gmail.com", to, []byte(fmt.Sprint(msg)))
 
 		if err != nil {
