20a18bff72e5820dc7b1d2c2b49eee5d00c7f67b
Taras <147646967+Mikki535@users.noreply.github.com>
2024-03-18 12:49:30 +0200

    Update main.go
    Update backend in main.go

--- main.go
+++ main.go
@@ -16,15 +16,16 @@ func main() {
 
 func HomeFunc(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+		ownGmail := r.FormValue("ownGmail")
+		secretKEY := r.FormValue("KEY")
 		gmail := r.FormValue("gmail")
 		message := r.FormValue("message")
-
-		auth := smtp.PlainAuth("", "ENTERGMAIL@gmail.com", "YOU-SECRET-CODE", "smtp.gmail.com")
+		auth := smtp.PlainAuth("", ownGmail, secretKEY, "smtp.gmail.com")
 
 		to := []string{gmail}
 		msg := message
 
-		err := smtp.SendMail("smtp.gmail.com:587", auth, "ENTERGMAIL@gmail.com", to, []byte(fmt.Sprint(msg)))
+		err := smtp.SendMail("smtp.gmail.com:587", auth, ownGmail, to, []byte(fmt.Sprint(msg)))
 
 		if err != nil {
 			http.Error(w, "ERROR! \n StatusBadRequest", http.StatusBadRequest)
